f4c55c98d002d60883b8d42be4ee6d10861938c6
Jonas <mechamojonas@gmail.com>
2022-11-22 09:43:01 -0300

    comentarios adicionados

--- src/client/Interface.java
+++ src/client/Interface.java
@@ -21,14 +21,17 @@ public class Interface {
 				
 				try {
 					tarefa = new Tarefa(nomeAluno, nomeTarefa + ".txt");
+					
+					//listaAlunos eh uma lista com o nome dos subdiretorios de ./Exemplos/
 					String[] listaAlunos = new File("Exemplos/").list();
-					
 					List<Tarefa> outrasSubmissoes = new ArrayList<Tarefa>();
-					
 					for(String aluno : listaAlunos) {
 						outrasSubmissoes.add(new Tarefa(aluno, nomeTarefa + ".txt"));
 					}
-					
+					//O CODIGO ABAIXO DEVE SER TRANSFERIDO PRA OUTRA CLASSE
+					//PARA RESPEITAR O PRINCIPIO DA SINGLE RESPONSABILITY
+					//ALEM DISSO, ELE NAO FUNCIONA, ESTA PRODUZINDO NULLPOINTEREXCEPTION
+					//porem, eh apenas um metodo de comparacao provisorio
 					for(Tarefa t : outrasSubmissoes) {
 						Map<String, Double> relatorio = verificador.comparaParagrafos(tarefa, t);
 						if (t.aluno != nomeAluno) {
